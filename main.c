ab0623565f06feebe204c1fe19bd0b327b863875
3akare <bakaredavid007@gmail.com>
2025-04-12 21:00:27 +0100

    feat: read from file, and add characters to buffer

--- main.c
+++ main.c
@@ -2,7 +2,19 @@
 
 Mode mode = NORMAL;
 
-int main(int argc, char* argv[]){
+int read_from_file(const char* filename, char *buffer, size_t buffer_s){
+    int ch = 0;
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) return 0;
+    while ((ch = fgetc(file)) != EOF){
+	buffer[buffer_s++] = ch;
+	addch(ch); 
+    }
+    fclose(file); 
+    return 1;
+}
+
+int main(int argc, const char* argv[]){
     initscr();
     noecho();
     raw();
@@ -20,12 +32,17 @@ int main(int argc, char* argv[]){
     mvprintw(max_y - 1, 0, stringify_mode(mode));
     move(y, x);
 
+    if (argc != 2 || argv[1] == NULL){
+	printf("Error: add a filename");
+	return 0;
+    } else read_from_file(argv[1], buffer, buffer_s);
+
     while ((ch = getch())){	
 	switch(mode){
 	    case NORMAL:
 		if (ch == 'q') exit = 1;
 		else if (ch == ctrl('s')){
-		    FILE *file = fopen("test.txt", "wb");
+		    FILE *file = fopen(argv[1], "wb");
 		    fwrite(buffer, 1, buffer_s, file);
 		    fclose(file);
 		    exit = 1;
