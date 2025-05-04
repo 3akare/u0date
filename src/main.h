3569c6162435487d7b24d54289329c98a9b9128c
3akare <bakaredavid007@gmail.com>
2025-05-04 16:07:21 +0100

    feat: add row and buffer functions. new buffer data definition

--- src/main.h
+++ src/main.h
@@ -10,13 +10,37 @@
 #define BACKSPACE 127
 #define ctrl(x) ((x) & 0x1f)
 
-typedef enum { NORMAL, INSERT } Mode;
+// type definitions
+typedef struct {
+    char *contents;
+    size_t size; // Length of text in the line
+    size_t capacity; // Allocated memory capacity for the line (includes null terminator)
+} row;
+
+typedef struct { 
+    row *rows;
+    size_t num_rows;
+    size_t capacity;
+} buffer;
+
+typedef enum { NORMAL, INSERT } EditorMode;
+typedef enum { MODE, INFO } Status;
 
 /* functions */
-const char* stringify_mode(Mode mode);
+const char* stringify_mode(EditorMode mode);
 
 /* file functions */
 int read_from_file(const char *filename, char *buffer, size_t buffer_s);
 int save_to_file(const char *filename, char *buffer, size_t buffer_s);
+
+/* row functions */
+void init_row(row *r);
+void free_row(row *r);
+void ensure_row_capacity(row *r, size_t needed);
+
+/* buffer functions */
+void init_buffer(buffer *buf);
+void free_buffer(buffer *buf);
+void ensure_buffer_capacity(buffer *buf, size_t needed_capacity);
 
 #endif
---
+++ src/row_funcs.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+void init_row(row *r) {
+	r->size = 0;
+	r->capacity = 1;
+	r->contents = malloc(r->capacity);
+	if (!r->contents) {
+		perror("malloc failed");
+		exit(EXIT_FAILURE);
+	}
+}
+
+void free_row(row *r) {
+	free(r->contents);
+	r->contents = NULL;
+	r->size = 0;
+	r->capacity = 0;
+}
+
+void ensure_row_capacity(row *r, size_t needed) {
+	size_t required_capacity = needed + 1;
+	if (r->capacity < required_capacity) {
+		size_t new_capacity = r->capacity == 0 ? 16 : r->capacity;
+		while (new_capacity < required_capacity) {
+			new_capacity *= 2;
+		}
+		char *new_contents = realloc(r->contents, new_capacity);
+		if (!new_contents) {
+			perror("realloc failed");
+			exit(EXIT_FAILURE);
+		}
+		r->capacity = new_capacity;
+		r->contents = new_contents;
+	}
+}
