e9618e26528e113dd542dcdada50d7ee5121acb9
3akare <bakaredavid007@gmail.com>
2025-05-06 07:02:53 +0100

    build: :hammer: update build script

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 
 FILENAME="${1:-file.txt}"
 
-gcc src/*.c -lncurses -o out.o && ./out.o $FILENAME && rm *.o
+gcc -Iinclude $(find src -name "*.c") -lncurses -o build/u0date ;
+./build/u0date $FILENAME
 
 cat $FILENAME
 if [ $2 ]; then
